a9916b0decd2a2314f73bbc0ea5af5b71655f645
Imesman <imesman@proton.me>
2023-10-30 12:29:06 -0400

    Added GLFW3Example.csproj.

---
+++ examples/GLFW3Example/Program.cs
@@ -0,0 +1,23 @@
+﻿//ReSharper disable all
+using System.Runtime.InteropServices;
+using DynamicInterop;
+using NativeLibrary = DynamicInterop.NativeLibrary;
+
+Console.WriteLine(Glfw.Init());
+
+public static class Glfw
+{
+    public delegate bool glfwInit();
+    public static glfwInit Init { get; }
+    
+    static Glfw()
+    {
+        NativeLibrary library = NativeLibrary.Create();
+            PathResolver resolver = new PathResolver();
+            resolver.Add(new OSPath(OSPlatform.Windows, Architecture.X64, "runtimes/win-x64/glfw3.dll"));
+        
+            library.Load(resolver.Get());
+            
+            Init = library.GetFunction<glfwInit>("glfwInit");
+    }
+}
